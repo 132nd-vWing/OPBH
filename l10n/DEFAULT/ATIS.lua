1cd757bfebcaa5e8d2d329361e6874877fe27972
Evo <karlkapadia@gmail.com>
2020-06-30 16:28:47 +0400

    added CTLD, added Mi8, changed name of CVN73 group ships and removed HAWK system

---
+++ l10n/DEFAULT/ATIS.lua
@@ -0,0 +1,7 @@
+atisMinhad=ATIS:New(AIRBASE.PersianGulf.Al_Minhad_AB, 121.400)
+atisMinhad:SetRadioRelayUnitName("Radio Relay Minhad")
+atisMinhad:Start()
+
+atisKhaimah=ATIS:New("Ras Al Khaimah", 118.100)
+atisKhaimah:SetRadioRelayUnitName("Radio Relay Khaimah")
+atisKhaimah:Start()
