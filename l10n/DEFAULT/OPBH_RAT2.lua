1281d4f325455e32a79251f8158b8e19ffa13510
Evo <karlkapadia@gmail.com>
2020-08-15 16:38:00 +0400

    DAy 2 mission ready

--- l10n/DEFAULT/OPBH_RAT2.lua
+++ l10n/DEFAULT/OPBH_RAT2.lua
@@ -1,24 +1,24 @@
 
 _SETTINGS:SetPlayerMenuOff()
 
-local a380table = { 'Emirates', 'Qantas Airways', 'Korean Air' }
+local a380table = { 'Emirates', }
 
-EK380DXBA = SPAWN:New( "RAT EK380" )
+EK380DXBA = SPAWN:New( "RAT EK380 #IFF:5014UN" )
 EK380DXBA:InitLimit( 4, 500 )
 EK380DXBA:InitRepeatOnLanding()
 EK380DXBA:SpawnScheduled( 600, 0 )
 
-EY320DXBD = SPAWN:New( "RAT EY320" )
+EY320DXBD = SPAWN:New( "RAT EY320 #IFF:5013UN" )
 EY320DXBD:InitLimit( 4, 500 )
 EY320DXBD:InitRepeatOnLanding()
 EY320DXBD:SpawnScheduled( 600, 0 )
 
-YAK40DXBA = SPAWN:New( "RAT YAK40" )
+YAK40DXBA = SPAWN:New( "RAT YAK40 #IFF:5012UN" )
 YAK40DXBA:InitLimit( 4, 500 )
 YAK40DXBA:InitRepeatOnLanding()
 YAK40DXBA:SpawnScheduled( 600, 0 )
 
-EO320BNDA = SPAWN:New( "RAT EP320" )
+EO320BNDA = SPAWN:New( "RAT EP320 #IFF:5011UN" )
 EO320BNDA:InitLimit( 4, 500 )
 EO320BNDA:InitRepeatOnLanding()
 EO320BNDA:SpawnScheduled( 600, 0 )
